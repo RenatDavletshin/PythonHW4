0bf1b34c5633715f8bd78e19ba8c1b931c337537
RenatDavletshin <immortal1307@gmail.com>
2022-12-28 00:32:16 +0500

    Task4End

--- Task4.py
+++ Task4.py
@@ -1,26 +1,6 @@
 # Задана натуральная степень k. 
 # Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 
-# from random import choice
-
-
-# def polynomial(num: int):
-    
-#     poly = ""
-#     num_list = range(0, 100)
-
-#     with open("poly.txt", "a") as my_file:
-#         for i in range(num, 0, -1):
-#             value = choice(num_list)
-#             if value:
-#                 poly += f"{value}*x^{i} {choice('+-')} "
-
-#         my_file.write(f"{poly}{choice(num_list)} = 0\n")
-
-
-# for _ in range(3):
-#     polynomial(int(input("Укажите степень многочлена: ")))
-
 import random
 
 
